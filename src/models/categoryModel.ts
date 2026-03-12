08ac6df66005b45697d595d2fa07db97197fa89c
nakaay13 <karbrz01@easv365.dk>
2026-03-12 10:18:31 +0100

    Update categoryModel.ts

--- src/models/categoryModel.ts
+++ src/models/categoryModel.ts
@@ -1,15 +1,10 @@
+// models/categoryModel.ts
 import { Schema, model } from 'mongoose';
-import { Car } from '../interfaces/car';
+import { Category } from '../interfaces/category';
 
-const carSchema = new Schema<Car>({
-  brand: { type: String, required: true },
-  modelName: { type: String, required: true },
-  year: { type: Number, required: true },
-  pricePerDay: { type: Number, required: true },
-  available: { type: Boolean, default: true },
-  imageUrl: { type: String },
-  categoryId: { type: Schema.Types.ObjectId, ref: 'Category', required: true },
-  locationId: { type: Schema.Types.ObjectId, ref: 'Location', required: true }
+const categorySchema = new Schema<Category>({
+  name: { type: String, required: true },
+  description: { type: String }
 });
 
-export const CarModel = model<Car>('Car', carSchema);
+export const CategoryModel = model<Category>('Category', categorySchema);
