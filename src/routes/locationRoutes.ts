873d4c056f8ef1cdaf7f22a113a6aa5551430ba8
nakaay13 <karbrz01@easv365.dk>
2026-03-12 10:21:07 +0100

    Create locationRoutes.ts

---
+++ src/routes/locationRoutes.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+import {
+  createLocation,
+  getLocations,
+  getLocationById,
+  updateLocation,
+  deleteLocation
+} from '../controllers/locationController';
+
+const router = Router();
+
+// CRUD routes
+router.post('/', createLocation);
+router.get('/', getLocations);
+router.get('/:id', getLocationById);
+router.put('/:id', updateLocation);
+router.delete('/:id', deleteLocation);
+
+export default router;
