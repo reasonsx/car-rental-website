159e719b4c64f7ff71b4c6aede6f3863f5ed0365
Tymon Mikula <karbrz01@easv365.dk>
2026-03-30 21:42:08 +0200

    car card compoenenet

---
+++ frontend/src/app/components/car-card/car-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Car } from '../../models/car.model';
+
+@Component({
+  selector: 'app-car-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './car-card.component.html',
+  styleUrls: ['./car-card.component.scss']
+})
+export class CarCardComponent {
+  @Input() car!: Car;
+}
