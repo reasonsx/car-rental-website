c85e52a6191464e95c47fff8736e93b1ac2e7db0
Tymon Mikula <karbrz01@easv365.dk>
2026-03-30 21:43:05 +0200

    app forntent update

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './components/nav/nav.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  standalone: true,
+  imports: [NavComponent, RouterOutlet],
+  template: `
+    <app-nav></app-nav>
+    <div class="app-container">
+      <router-outlet></router-outlet>
+    </div>
+  `,
+  styleUrls: ['./app.scss']
 })
 export class App {
   protected readonly title = signal('frontend');
 }
+
