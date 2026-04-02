d4e1dc3a3abc390bae4d57b79c7413ae7f7ccf7b
Karolina <karbrz01@easv365.dk>
2026-04-02 18:57:11 +0200

    bookings adin component added

---
+++ frontend/src/app/pages/admin-dashboard/components/admin-bookings/admin-bookings.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-admin-bookings',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './admin-bookings.component.html',
+  styleUrls: ['./admin-bookings.component.scss']
+})
+export class AdminBookingsComponent {
+
+}
