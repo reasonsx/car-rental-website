af78d751c4eb12b6e2e16fd65f8d5c1eb36317d6
Karolina <karbrz01@easv365.dk>
2026-04-02 18:56:35 +0200

    locations component

---
+++ frontend/src/app/pages/admin-dashboard/components/admin-locations/admin-locations.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-admin-locations',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './admin-locations.component.html',
+  styleUrls: ['./admin-locations.component.scss']
+})
+export class AdminLocationsComponent {
+
+}
