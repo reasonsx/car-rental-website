dd29906992d80c57b0474ec110380a328f2e54ce
Karolina <karbrz01@easv365.dk>
2026-04-02 18:56:47 +0200

    category admin component added

---
+++ frontend/src/app/pages/admin-dashboard/components/admin-categories/admin-categories.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-admin-categories',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './admin-categories.component.html',
+  styleUrls: ['./admin-categories.component.scss']
+})
+export class AdminCategoriesComponent {
+
+}
