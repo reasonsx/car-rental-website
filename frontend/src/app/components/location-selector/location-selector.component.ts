b216140798da78ededeeeba8e837275ddd585104
reasonsx <kamkre01@easv365.dk>
2026-04-15 16:32:27 +0200

    updates

--- frontend/src/app/components/location-selector/location-selector.component.ts
+++ frontend/src/app/components/location-selector/location-selector.component.ts
@@ -1,13 +1,15 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SelectModule } from 'primeng/select';
+
 import { Location } from '../../models/location.model';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-location-selector',
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './location-selector.component.html',
-  styleUrls: ['./location-selector.component.scss']
+  imports: [CommonModule, SelectModule, FormsModule],
+  templateUrl: './location-selector.component.html'
 })
 export class LocationSelectorComponent {
   @Input() locations: Location[] = [];
