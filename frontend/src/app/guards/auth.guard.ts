0eae5f116488586eca39f0ce31f3e67edebe8715
reasonsx <kamkre01@easv365.dk>
2026-04-16 08:16:44 +0200

    inject instead of constructor

--- frontend/src/app/guards/auth.guard.ts
+++ frontend/src/app/guards/auth.guard.ts
@@ -1,13 +1,12 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { CanActivate, Router, UrlTree } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
-  constructor(
-    private authService: AuthService,
-    private router: Router
-  ) {}
+
+  private authService = inject(AuthService);
+  private router = inject(Router);
 
   canActivate(): boolean | UrlTree {
     if (!this.authService.isAuthenticated()) {
