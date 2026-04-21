ae02bc66179dd0eb948327a6e0b35f4aeab73d31
Karolina <karbrz01@easv365.dk>
2026-04-21 10:55:25 +0200

    commnts

--- frontend/src/app/interceptors/auth.interceptor.ts
+++ frontend/src/app/interceptors/auth.interceptor.ts
@@ -1,7 +1,7 @@
 import { HttpInterceptorFn } from "@angular/common/http";
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const token = localStorage.getItem("authToken");
+  const token = localStorage.getItem("authToken"); //optimase this!!!!
 
   if (!token) return next(req);
 
