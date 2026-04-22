c6b8b5fd63330165cd3976c399c79689aa5ea21c
nakaay13 <karbrz01@easv365.dk>
2026-04-22 16:01:45 +0200

    is deleted added to model

--- backend/src/modules/user/user.model.ts
+++ backend/src/modules/user/user.model.ts
@@ -6,6 +6,7 @@ const userSchema = new Schema(
     email: { type: String, required: true, unique: true, lowercase: true },
     password: { type: String, required: true },
     isAdmin: { type: Boolean, default: false },
+    isDeleted: { type: Boolean, default: false },
   },
   {
     timestamps: true,
